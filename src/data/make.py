356254943eabe8f488df1b0bad965adbe8bf0a91
Georgios Vlassis <gvlassis@mailbox.org>
2025-07-10 13:50:04 +0200

    Made make.py easier

--- src/data/make.py
+++ src/data/make.py
@@ -4,7 +4,6 @@ import utils_data
 import torch
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("DIR", help="The directory which will contain train_X.pt, train_Y.pt, val_X.pt, val_Y.pt, test_X.pt, test_Y.pt", type=os.path.abspath)
 parser.add_argument("--dataset", choices=utils_data.DATASETS, default="climbmix10m")
 parser.add_argument("--tokenizer_type", choices=utils_data.TOKENIZER_TYPES, help="Tokenizer library to use", default="tokenmonster")
 parser.add_argument("--tokenizer", help="Name/URL/File of the tokenizer", default="https://huggingface.co/gvlassis/tokenmonster/resolve/main/englishcode-32000-strict-nocapcode-v1-eot%3D14199.vocab?download=true")
@@ -12,7 +11,12 @@ parser.add_argument("--eot_id", help="End-Of-Text token id", type=int, default=1
 parser.add_argument("--cores", help="CPU cores used by datasets.Dataset.map(num_proc=).", type=int, default=os.cpu_count()//2)
 args=parser.parse_args()
 
-os.makedirs(args.DIR, exist_ok=True)
+script_path = os.path.abspath(__file__)
+data_path = os.path.dirname(script_path)
+src_path = os.path.dirname(data_path)
+root_path = os.path.dirname(src_path)
+dataset_path = f"{root_path}/{args.dataset}"
+os.makedirs(dataset_path, exist_ok=True)
 
 if args.tokenizer_type=="tokenizers":
     import transformers
@@ -30,11 +34,11 @@ test_X, test_Y = utils_data.dataset_to_tensors(test_dataset, args.tokenizer_type
 val_X, val_Y = utils_data.dataset_to_tensors(val_dataset, args.tokenizer_type, tokenizer, args.eot_id, args.cores)
 
 print("💾 Saving splits")
-torch.save(train_X, args.DIR+"/train_X.pt")
-if train_Y is not None: torch.save(train_Y, args.DIR+"/train_Y.pt")
-torch.save(val_X, args.DIR+"/val_X.pt")
-if val_Y is not None: torch.save(val_Y, args.DIR+"/val_Y.pt")
-torch.save(test_X, args.DIR+"/test_X.pt")
-if test_Y is not None: torch.save(test_Y, args.DIR+"/test_Y.pt")
+torch.save(train_X, dataset_path+"/train_X.pt")
+if train_Y is not None: torch.save(train_Y, dataset_path+"/train_Y.pt")
+torch.save(val_X, dataset_path+"/val_X.pt")
+if val_Y is not None: torch.save(val_Y, dataset_path+"/val_Y.pt")
+torch.save(test_X, dataset_path+"/test_X.pt")
+if test_Y is not None: torch.save(test_Y, dataset_path+"/test_Y.pt")
 
 print("🍾 Done!")
